f4cdba5a931c1d2e2d0603e180ac8756a9982054
Tanielian V. Barreira <tanielianvb@gmail.com>
2017-09-07 08:54:22 -0300

    Added extensions to int. Issues: #6

---
+++ src/Super.Extensions/Int.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Super.Extensions
+{
+    /// <summary>
+    /// Extensions to int.
+    /// </summary>
+    public static class IntExtensions
+    {
+        #region Math
+
+        /// <summary>
+        /// Returns a specified number raised to the specified power.
+        /// </summary>
+        /// <param name="x">A int number to be raised to a power.</param>
+        /// <param name="y">A double-precision floating-point number that specifies a power.</param>
+        /// <returns>The number x raised to the power y.</returns>
+        public static double Pow(this int x, double y) => Math.Pow(x, y);
+
+        #endregion
+    }
+}
---
+++ test/Super.Extensions.Tests/Int.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace Super.Extensions.Tests
+{
+    public class IntExtensionsTests
+    {
+        #region Math
+
+        [InlineData(-100, -100)]
+        [InlineData(-1, -1)]
+        [InlineData(0, 0)]
+        [InlineData(1, 1)]
+        [InlineData(100, 100)]
+        [Trait("int", "Pow")]
+        [Theory(DisplayName = "Pow")]
+        public void PowTest(int x, int y) => Assert.Equal(Math.Pow(x, y), x.Pow(y));
+
+        #endregion
+    }
+}
