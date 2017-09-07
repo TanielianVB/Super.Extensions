e76aaf3900985d2c98d4c029998fdac9fecf4cfc
Tanielian V. Barreira <tanielianvb@gmail.com>
2017-09-07 07:39:56 -0300

    Added tests to the extensions to string. Issues: #1, #2

---
+++ test/Super.Extensions.Tests/String.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+
+namespace Super.Extensions.Tests
+{
+    public class StringExtensionsTests
+    {
+        #region String
+
+        [InlineData(null)]
+        [InlineData("")]
+        [Trait("string", "IsNullOrEmpty")]
+        [Theory(DisplayName = "IsNullOrEmpty")]
+        public void IsNullOrEmptyTest(string value) => Assert.True(value.IsNullOrEmpty());
+
+        [InlineData("value")]
+        [Trait("string", "IsNullOrEmpty")]
+        [Theory(DisplayName = "IsNullOrEmpty")]
+        public void NotIsNullOrEmptyTest(string value) => Assert.False(value.IsNullOrEmpty());
+
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("   ")]
+        [Trait("string", "IsNullOrWhiteSpace")]
+        [Theory(DisplayName = "IsNullOrWhiteSpace")]
+        public void IsNullOrWhiteSpaceTest(string value) => Assert.True(value.IsNullOrWhiteSpace());
+
+        [InlineData("value")]
+        [Trait("string", "IsNullOrWhiteSpace")]
+        [Theory(DisplayName = "IsNullOrWhiteSpace")]
+        public void NotIsNullOrWhiteSpaceTest(string value) => Assert.False(value.IsNullOrWhiteSpace());
+
+        #endregion
+    }
+}
